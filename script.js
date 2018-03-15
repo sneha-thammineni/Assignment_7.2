7aa08bd7336bf302965f9145ebcc7b652cfcfe30
sneha-thammineni <36546445+sneha-thammineni@users.noreply.github.com>
2018-03-15 16:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+console.log('Using for loop');
+for (var i=1; i<=10; i++){var n = 5;var code = n * i; 
+  console.log(code);}
+
+console.log('Using while loop');
+var j = 1;
+while(j<=10) {
+   var m = 5;
+   var newCode = m * j;
+    j++;
+    console.log(newCode);}
