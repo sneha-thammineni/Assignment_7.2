47827a1db4ef554770467c513c1051d594cb034b
sneha-thammineni <36546445+sneha-thammineni@users.noreply.github.com>
2018-03-15 16:32:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (var i=1; i<=10; i++){var n = 5;var code = n * i;
   console.log(code);}
 
 console.log('Using while loop');
+// consider new variables j, m , newCode 
 var j = 1;
 while(j<=10) {
    var m = 5;
